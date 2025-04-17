b604867d389356bfafb67af5d20e12489405b00b
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2025-04-17 10:30:23 +0200

    in romanum

--- NumerusRomanus.js
+++ NumerusRomanus.js
@@ -1,26 +1,26 @@
-const NumerusRomanus = num =>
+const InRomanum = num =>
     num < 1   ? '' :
-    num >= 90 ? 'XC' + NumerusRomanus(num - 90) :
-    num >= 50 ? 'L'  + NumerusRomanus(num - 50) :
-    num >= 40 ? 'XL' + NumerusRomanus(num - 40) :
-    num >= 10 ? 'X'  + NumerusRomanus(num - 10) :
-    num >= 9  ? 'IX' + NumerusRomanus(num - 9) :
-    num >= 5  ? 'V'  + NumerusRomanus(num - 5) :
-    num >= 4  ? 'IV' + NumerusRomanus(num - 4) :
-    'I' + NumerusRomanus(num - 1);  
+    num >= 90 ? 'XC' + InRomanum(num - 90) :
+    num >= 50 ? 'L'  + InRomanum(num - 50) :
+    num >= 40 ? 'XL' + InRomanum(num - 40) :
+    num >= 10 ? 'X'  + InRomanum(num - 10) :
+    num >= 9  ? 'IX' + InRomanum(num - 9) :
+    num >= 5  ? 'V'  + InRomanum(num - 5) :
+    num >= 4  ? 'IV' + InRomanum(num - 4) :
+    'I' + InRomanum(num - 1);  
 
-console.log(NumerusRomanus(0)); // "" - just below lower bound (not valid in Roman numerals)
-console.log(NumerusRomanus(1)); // "I" - lower bound
-console.log(NumerusRomanus(2)); // "II" - just above lower bound
-console.log(NumerusRomanus(3)); // "III" - highest number of same symbol in a row
-console.log(NumerusRomanus(4)); // "IV" - special case
-console.log(NumerusRomanus(5)); // "V" - next base value
-console.log(NumerusRomanus(9)); // "IX" - special case
-console.log(NumerusRomanus(10)); // "X" - transition to tens
-console.log(NumerusRomanus(39)); // "XXXIX" - just below XL
-console.log(NumerusRomanus(40)); // "XL" - edge of 40
-console.log(NumerusRomanus(49)); // "XLIX" - edge case combining XL and IX
-console.log(NumerusRomanus(50)); // "L"
-console.log(NumerusRomanus(89)); // "LXXXIX"
-console.log(NumerusRomanus(90)); // "XC"
-console.log(NumerusRomanus(99)); // "XCIX"
+console.log(InRomanum(0)); // "" - just below lower bound (not valid in Roman numerals)
+console.log(InRomanum(1)); // "I" - lower bound
+console.log(InRomanum(2)); // "II" - just above lower bound
+console.log(InRomanum(3)); // "III" - highest number of same symbol in a row
+console.log(InRomanum(4)); // "IV" - special case
+console.log(InRomanum(5)); // "V" - next base value
+console.log(InRomanum(9)); // "IX" - special case
+console.log(InRomanum(10)); // "X" - transition to tens
+console.log(InRomanum(39)); // "XXXIX" - just below XL
+console.log(InRomanum(40)); // "XL" - edge of 40
+console.log(InRomanum(49)); // "XLIX" - edge case combining XL and IX
+console.log(InRomanum(50)); // "L"
+console.log(InRomanum(89)); // "LXXXIX"
+console.log(InRomanum(90)); // "XC"
+console.log(InRomanum(99)); // "XCIX"
---
+++ display.md
@@ -0,0 +1,23 @@
+190 characters allowed
+
+this is 406
+```JavaScript
+const InRomanum = num =>
+    num < 1   ? '' :
+    num >= 90 ? 'XC' + InRomanum(num - 90) :
+    num >= 50 ? 'L'  + InRomanum(num - 50) :
+    num >= 40 ? 'XL' + InRomanum(num - 40) :
+    num >= 10 ? 'X'  + InRomanum(num - 10) :
+    num >= 9  ? 'IX' + InRomanum(num - 9) :
+    num >= 5  ? 'V'  + InRomanum(num - 5) :
+    num >= 4  ? 'IV' + InRomanum(num - 4) :
+    'I' + InRomanum(num - 1); 
+```
+182 chars:
+```
+const InRomanum = num =>
+    num < 1   ? '' :
+    num >= 5  ? 'V'  + InRomanum(num - 5) :
+    num >= 4  ? 'IV' + InRomanum(num - 4) :
+    'I' + InRomanum(num - 1); 
+```
